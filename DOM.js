52e6959f29d7dea12a500923701f7de59d0ce5ef
Andrew Caldwell <andrewcaldwell@Andrews-MacBook-Pro.local>
2021-01-14 23:57:08 -0500

    files can now be written two with communication between front end and backend thread

--- DOM.js
+++ DOM.js
@@ -1,12 +1,25 @@
-(function(){
-    function $(elem){
-        return document.querySelector(elem);
-    }  
+let ipc = require('electron').ipcRenderer;
 
-    function buildEntry(title, content, uniqueID){
-        let strippedContent = content.substring(0,25) + "..."
+
+(function () {
+
+    // test sending data to other process
+
+
+
+
+
+
+
+    function $(elem) {
+        return document.querySelector(elem);
+    }
+
+    function buildEntry(title, content, uniqueID) {
+        let strippedContent = content.substring(0, 25) + "...";
+        let mUID = uniqueID.split(" ").join("-");
         let inner = `
-        <div class="SN-main-sidebar-elem ${uniqueID}">
+        <div class="SN-main-sidebar-elem ${mUID}">
                     <div class="SN-main-sidebar-elem-title">${title}</div>
                     <div class="SN-main-sidebar-elem-content-preview">${strippedContent}</div>
                 </div>
@@ -15,9 +28,9 @@
         return inner;
     }
 
-    
 
-    $('.SN-main-sidebar-controls-plus').addEventListener('click', ()=>{
+
+    $('.SN-main-sidebar-controls-plus').addEventListener('click', () => {
         let popup = document.createElement('div');
         popup.innerHTML = `
         <div class="popup-container">
@@ -30,7 +43,7 @@
         </div>
         `;
         document.body.appendChild(popup);
-        $('.popup-close').addEventListener('click', () =>{
+        $('.popup-close').addEventListener('click', () => {
             $('.popup-container').remove();
         });
 
@@ -44,13 +57,34 @@
             let element = buildEntry(title, content, uniqueID);
             let elemWrapper = document.createElement('div');
             elemWrapper.innerHTML = element;
+
+            let saveData = {
+                uid: uniqueID,
+                notetitle: title,
+                body: content
+            };
+
+            //call to main process to write file
+            ipc.send('write-Note', saveData);
+            ipc.on('write-Note-Reply', (event, args) => {
+                console.log(args);
+            });
+            //end call to main process
             $(".SN-main-sidebar-content").appendChild(elemWrapper);
-            
+            elemWrapper.addEventListener('click',(e)=> {
+                let target = e.currentTarget;
+                let targetClassList = target.querySelector('.SN-main-sidebar-elem').classList;
+                console.log(targetClassList);
+            });
+
         });
     });
 
 
-    
+
+
+
+
 
 
 
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
+const fs = require('fs');
 
 function createWindow () {
   const win = new BrowserWindow({
@@ -24,4 +25,56 @@ app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow()
   }
+})
+
+
+ipcMain.on('write-Note', (event, args) => {
+  console.log(args);
+  let data = {
+    title : args.notetitle,
+    content : args.body
+  };
+
+  
+
+  let dataReadyToWrite = JSON.stringify(data);
+  let filepath = args.uid.split(" ").join("-");
+  let date = new Date();
+  let now = date.getTime();
+
+  let log = {
+    fp: `${filepath}.json`,
+    updated: now
+  };
+
+  fs.writeFile(`files/${filepath}.json`, dataReadyToWrite, (err) => {
+    if (err) {
+      console.log(err);
+      event.sender.send('write-Note-Reply', {success: false});
+    } 
+    else{
+      fs.readFile('files/track.json','utf-8',(err, data) => {
+        if(err){
+          console.log(err);
+          event.sender.send('write-Note-Reply', {success: false});
+        }
+        else{
+          let parsedData = JSON.parse(data);
+          parsedData.unshift(log);
+          let stringToPush = JSON.stringify(parsedData);
+          fs.writeFile('files/track.json', stringToPush, (err)=>{
+            if(err){
+              console.log(err);
+            }
+            else{
+              console.log('written correctly');
+              event.sender.send('write-Note-Reply', {success : true});
+            }
+          });
+        }
+      });
+    }
+    
+  });
+  
 })
